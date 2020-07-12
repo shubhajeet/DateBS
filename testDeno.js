9efc4861713333f166852546ebcb2373fcb12c76
Sujit Maharjan <Sujit.Maharjan@logpoint.com>
2020-07-12 16:43:13 +0545

    testDeno modified to use rawgithub url

--- testDeno.js
+++ testDeno.js
@@ -1,4 +1,4 @@
-import { DateBS } from "./DateBS/DateBS.ts";
+import { DateBS } from "https://raw.githubusercontent.com/shubhajeet/DateBS/master/src/index.ts";
 
 let today = DateBS.fromAD();
 console.log(today);
